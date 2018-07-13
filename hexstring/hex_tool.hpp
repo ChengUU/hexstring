ac299dfef606c937060028fec385c6b1bb0da0d1
liuguang <67579722@qq.com>
2018-07-13 13:12:43 +0800

    添加debug工具

--- hexstring/hex_tool.hpp
+++ hexstring/hex_tool.hpp
@@ -3,9 +3,15 @@
 #include <string>
 using std::vector;
 using std::string;
+using std::size_t;
+
 //char数组转换
 void hexToBytes(const char * input, vector<char>& output);
 void bytesToHex(const vector<char>& input, string & output, bool upperCase = true);
 //c字符串转换
 void hexToBytes(const char * input, string& output);
 void bytesToHex(const char * input, string & output, bool upperCase = true);
+//debug工具
+void hexStrToHexDebug(const char * hexStr, string& debugStr, size_t rowBytesCount = 16);
+void bytesToHexDebug(const vector<char>& rawBytes,string& debugStr,size_t rowBytesCount=16 , bool upperCase = true);
+void bytesToHexDebug(const char * rawBytes, string& debugStr, size_t rowBytesCount = 16, bool upperCase = true);
