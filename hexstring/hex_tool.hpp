bff1e4f3a5ca8ba6435c27abb6b978df3c64ba58
liuguang <67579722@qq.com>
2018-07-13 14:54:47 +0800

    ASCII字符在右侧显示

--- hexstring/hex_tool.hpp
+++ hexstring/hex_tool.hpp
@@ -12,6 +12,5 @@ void bytesToHex(const vector<char>& input, string & output, bool upperCase = tru
 void hexToBytes(const char * input, string& output);
 void bytesToHex(const char * input, string & output, bool upperCase = true);
 //debug¹¤¾ß
-void hexStrToHexDebug(const char * hexStr, string& debugStr, size_t rowBytesCount = 16);
 void bytesToHexDebug(const vector<char>& rawBytes,string& debugStr,size_t rowBytesCount=16 , bool upperCase = true);
 void bytesToHexDebug(const char * rawBytes, string& debugStr, size_t rowBytesCount = 16, bool upperCase = true);
