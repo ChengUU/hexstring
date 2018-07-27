01fd1339cdca46efb0287e0144ccada0c150e728
liuguang <67579722@qq.com>
2018-07-27 11:31:21 +0800

    fix

--- hexstring/hex_tool.hpp
+++ hexstring/hex_tool.hpp
@@ -5,14 +5,14 @@
 #include <stdexcept>
 
 //char数组转换
-void hexToBytes(const char * input, std::vector<char>& output);
-void bytesToHex(const std::vector<char>& input, std::string & output, bool upperCase = true);
+inline void hexToBytes(const char * input, std::vector<char>& output);
+inline void bytesToHex(const std::vector<char>& input, std::string & output, bool upperCase = true);
 //c字符串转换
-void hexToBytes(const char * input, std::string& output);
-void bytesToHex(const char * input, std::string & output, bool upperCase = true);
+inline void hexToBytes(const char * input, std::string& output);
+inline void bytesToHex(const char * input, std::string & output, bool upperCase = true);
 //debug工具
-void bytesToHexDebug(const std::vector<char>& rawBytes, std::string& debugStr, std::size_t rowBytesCount = 16, bool upperCase = true);
-void bytesToHexDebug(const char * rawBytes, std::string& debugStr, std::size_t rowBytesCount = 16, bool upperCase = true);
+inline void bytesToHexDebug(const std::vector<char>& rawBytes, std::string& debugStr, std::size_t rowBytesCount = 16, bool upperCase = true);
+inline void bytesToHexDebug(const char * rawBytes, std::string& debugStr, std::size_t rowBytesCount = 16, bool upperCase = true);
 
 //=================================================================
 
